4a939606a6b2cec8deb3735b6c35e1d76be25714
kcloze <leewayed@hotmail.com>
2012-03-12 22:21:04 +0800

    Change-Id: I819bb50dc49a515498bdca5f86671d854df8d9de

--- cms/views/layouts/column2.php
+++
@@ -1,9 +0,0 @@
-<?php $this->beginContent('/layouts/main'); ?>
-<div class="container">
-	<div class="">
-		<div id="content">
-			<?php echo $content; ?>
-		</div><!-- content -->
-	</div>
-</div>
-<?php $this->endContent(); ?>
